8f0b3cb0577269f5e052adbf8e7c4d54e849cde3
Sam-ggl <samuel.grulich@gmail.com>
2022-07-27 13:42:23 +0200

    changed rootdir to url

--- routes/auth/callback.tsx
+++ routes/auth/callback.tsx
@@ -1,16 +1,25 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, Handlers } from "$fresh/server.ts";
+
+// export const handler: Handlers = {
+//   async GET(req, ctx) {
+
+//   } 
+// }
 
 export default function CallbackPage(props: PageProps) {
   const params = new URLSearchParams(props.url.search);
   console.log(params.get("code"));
   console.log(params.get("state"));
-  
+
   return (
     <div>
       You're in!
+      <span>{params.get("code")}</span>
+      <span>{params.get("span")}</span>
+      <button>Continue</button>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,6 +4,11 @@ import { tw } from "@twind";
 import Menu from "@root/components/Menu.tsx";
 
 export default function Home() {
+  if(!localStorage.getItem("x"))  
+    localStorage.setItem("x", "10");
+  
+  localStorage["x"]++;
+
   return (
     <div class={tw('')}>
       <Menu />
@@ -12,6 +17,8 @@ export default function Home() {
         <h1>Home</h1>
         <p>Welcome! Spotfio lets you track your spotify playlists and listen to music with friends</p>
       </main>
+
+      <span>{localStorage.getItem("x")}</span>
     </div>
   );
 }
