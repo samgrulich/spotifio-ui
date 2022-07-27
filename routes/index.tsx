000b46c50b599ffe645ed38f6a9d07a352127762
Sam-ggl <samuel.grulich@gmail.com>
2022-07-27 14:23:42 +0200

    Removed localStorage
    removed localstorage access from index

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,11 +4,6 @@ import { tw } from "@twind";
 import Menu from "../components/Menu.tsx";
 
 export default function Home() {
-  if(!localStorage.getItem("x"))  
-    localStorage.setItem("x", "10");
-  
-  localStorage["x"]++;
-
   return (
     <div class={tw('')}>
       <Menu />
@@ -17,8 +12,6 @@ export default function Home() {
         <h1>Home</h1>
         <p>Welcome! Spotfio lets you track your spotify playlists and listen to music with friends</p>
       </main>
-
-      <span>{localStorage.getItem("x")}</span>
     </div>
   );
 }
